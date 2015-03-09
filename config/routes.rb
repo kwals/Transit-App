3110051e1adf96afa956f56799861b506ca78d38
jodonne91 <jodonne91@gmail.com>
2015-03-09 17:22:42 -0400

    Updates to template and request calls

--- config/routes.rb
+++ config/routes.rb
@@ -6,6 +6,7 @@ Rails.application.routes.draw do
   get '/'       => 'staticpages#home',   as: 'home'
   get '/ubers'   => 'ubers#index',         as: 'ubers'
 
+
   # The priority is based upon order of creation: first created -> highest priority.
   # See how all your routes lay out with "rake routes".
 
