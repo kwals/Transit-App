f6f26dcfd1c09af1580c59ee44462959f56bb3b8
Katie Walsh <katie.katiewalsh@gmail.com>
2015-03-09 16:49:05 -0400

    Added Bikeshare api

--- app/models/bikeshare.rb
+++ app/models/bikeshare.rb
@@ -1,22 +1,63 @@
 class BikeShare
-# We are going to need to use Nokogiri to parse the XML
-# use the slop method to make it into a hash.
-# require 'open-uri'
-# doc = Nokogiri::HTML(open("http://www.capitalbikeshare.com/data/stations/bikeStations.xml"))
+require 'HTTParty'
+require "pry"
   
-  def radius
-    # This should calculate a range for latitude
-    # Then calculate a range for longitude 
+  @radius_number = 0.0025
+
+  def self.scrape
+    info = HTTParty.get("http://www.capitalbikeshare.com/data/stations/bikeStations.xml")
+    stations_array = info["stations"]["station"]
+  end 
+
+  
+  def self.bikes_nearby(lat, long) 
+    stations_array = self.scrape
+    lat_max = lat.to_f + @radius_number.to_f
+    lat_min = lat.to_f - @radius_number.to_f
+    long_max = long.to_f + @radius_number.to_f
+    long_min = long.to_f - @radius_number.to_f
+    nearby_stations = []
+    # Each station will be given as a hash
+
+    stations_array.each do |s|
+      this_lat = s["lat"].to_f
+      this_long = s["long"].to_f
+      if (lat_min < this_lat) && (this_lat < lat_max)
+        if (long_min < this_long) && (this_long < long_max)
+        place_hash = {
+          id: s["id"],
+          name: s["name"],
+          bikes: s["nbBikes"],
+          empty: s["nbEmptyDocks"],
+          dock_lat: s["lat"],
+          dock_long: s["long"]
+        }
+        nearby_stations.push(place_hash)
+        end
+      end
+      nearby_stations
+    end
+    nearby_stations
   end
 
-  def get_bike_stations
-    # This should parse the XML and return stations
-    # that are within BOTH the lat AND long range
+  def bike_station_info id_number
+    stations_array = self.scrape
+    stations_array.each do |s|
+      if s["id"].to_i == id_number.to_i
+        place_hash = {
+          id: s["id"],
+          name: s["name"],
+          bikes: s["nbBikes"],
+          empty: s["nbEmptyDocks"],
+          dock_lat: s["lat"],
+          dock_long: s["long"]
+        }
+        place_hash
+        end
+      place_hash
+    end
+    place_hash
   end
 
-  def bike_station_info
-    # THis should search for a station with a station ID
-    # And return location, number of bikes, number of empty slots
-  end
 
 end
