2f5638bc4b01a851990d441d5a0e3241c662ba84
Katie Walsh <katie.katiewalsh@gmail.com>
2015-03-09 17:20:25 -0400

    Add a very important colon

--- app/controllers/bike_share_controller.rb
+++ app/controllers/bike_share_controller.rb
@@ -1,7 +1,7 @@
 class BikeShareController < ApplicationController
 
   def index (lat, long)
-    render json: BikeShare.bikes_nearby(lat: params[lat], long: params[:long])
+    render json: BikeShare.bikes_nearby(lat: params[:lat], long: params[:long])
   end
 
 end
