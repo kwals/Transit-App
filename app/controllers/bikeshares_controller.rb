71ff31a4bfa114ec069f6955f8e6b79e8b1368ea
Katie Walsh <katie.katiewalsh@gmail.com>
2015-03-09 18:13:13 -0400

    Hardcode bus endpoint

--- app/controllers/bikeshares_controller.rb
+++ app/controllers/bikeshares_controller.rb
@@ -1,7 +1,7 @@
 class BikesharesController < ApplicationController
 
   def index 
-    render json: Bikeshare.bikes_nearby(lat: params[:lat], long: params[:long])
+    render json: Bikeshare.bikes_nearby(params)
   end
 
 end
---
+++ app/controllers/buses_controller.rb
@@ -0,0 +1,6 @@
+class BusesController < ApplicationController
+
+  def index
+    render json: "Bus info should be here."
+  end
+end
