6fbaa5506f1eeaa8eed69bcf2c18987304cb9c1d
Wonman Lee <wonmanlee@gmail.com>
2015-03-10 21:50:38 -0400

    Added favorites json endpoint

--- app/controllers/favorites_controller.rb
+++ app/controllers/favorites_controller.rb
@@ -1,13 +1,24 @@
 class FavoritesController < ApplicationController
   before_action :authenticate_user!
   
+  def index
+    h = {}
+    h["favorites"] = []
+    current_user.favorites.each do |favs|
+      f = {}
+      f["name"] = favs.name
+      f["lat"] = favs.lat
+      f["long"] = favs.long
+      h["favorites"] << f
+    end
+    @favorites = h
+    render json: @favorites
+  end
+
   def create
     f = current_user.favorites.new(favorite_params)
     f.save
     render json: "You added #{f.name} as a favorite place."
-  end
-
-  def index
   end
 
 private
